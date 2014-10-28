fad76d41bb1a25bc9cd9db5f77825650bb698479
Jason S. Jones <jsjones96@gmail.com>
2014-10-27 21:25:02 -0700

    Minor refactor in index.html and the main app.js file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,30 @@
-'use strict';
+(function () {
+  'use strict';
 
-var app = angular.module('app', ['ngRoute']);
+  var app = angular.module('app', ['ngRoute']);
 
-app.config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-        templateUrl: 'views/mainview.html',
-        controller: 'MainController',
-        controllerAs: 'main',
-        resolve: {
-            getData: function(withdrawalService) {
-                return withdrawalService.initJSONData('data.json');
-            }
-        }
-    })
-    .when('/atmdetails', {
-        templateUrl: 'views/atm-details-view.html',
-        controller: 'ATMController as atm'
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
-});
+  app.config(function($routeProvider) {
+      $routeProvider
+      .when('/', {
+          templateUrl: 'views/mainview.html',
+          controller: 'MainController',
+          controllerAs: 'main'
+        /*
+          resolve: {
+              getData: function(withdrawalService) {
+                  return withdrawalService.initJSONData('data.json');
+              }
+          }
+        */
+      })
+      .when('/atmdetails', {
+          templateUrl: 'views/atm-details-view.html',
+          controller: 'ATMController',
+          controllerAs: 'atm'
+      })
+      .otherwise({
+          redirectTo: '/'
+      });
+  });
+    
+}());
